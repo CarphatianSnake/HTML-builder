d3e8df42d936e0e4c4c74136b2dc2c8b3bf8809d
unknown <sunsn4ke@gmail.com>
2022-11-01 23:53:24 +0200

    finish: finish task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,12 +2,45 @@ const fs = require('fs');
 const path = require('path');
 const { stdin, stdout } = process;
 
+const textFilePath = path.join(__dirname, 'text.txt');
+
 const deleteFlag = process.argv[2];
 if (deleteFlag === '-d') return removeFile();
 
+const output = fs.createWriteStream(textFilePath);
+
+init();
+
+const rl = require("readline").createInterface({
+  input: stdin,
+  output: stdout
+});
+
+stdout.write('Please enter any text:\n');
+
+rl.on('line', data => {
+    
+  if (data === 'exit') {
+    process.exit(0);
+  } else {
+    output.write(`${data}\n`);
+  }
+
+});
+
+rl.on("SIGINT", () => {
+  process.emit("SIGINT");
+});
+
+process.on("SIGINT", () => {
+  process.exit(0);
+});
+
+process.on('exit', () => stdout.write('\nGood luck!'));
+
 function init() {
   fs.writeFile(
-    path.join(__dirname, 'text.txt'),
+    textFilePath,
     '',
     error => {
       if (error) return console.error(error.message);
@@ -17,12 +50,10 @@ function init() {
 
 function removeFile() {
   fs.unlink(
-    path.join(__dirname, 'text.txt'),
+    textFilePath,
     error => {
       if (error) return console.error(error.message);
       console.log('File deleted');
     }
   );
 }
-
-init();
