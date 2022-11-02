38ca79319e4bd0e1fb7a489ec0e3a13ac2b254b1
unknown <sunsn4ke@gmail.com>
2022-11-02 15:27:08 +0200

    finish: finish task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,25 +2,70 @@ const path = require('path');
 const fs = require('fs');
 
 const filesPath = path.join(__dirname, 'files');
-
-fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, error => {
-  if (error) return console.error(error.message);
-});
-
 const copyFilesPath = path.join(__dirname, 'files-copy');
 
-fs.readdir(filesPath, (error, files) => {
+copyDir();
 
+function copyDir() {
+
+  fs.readdir(
+    filesPath,
+    (error, files) => {
+  
+      isErrorMessage(error);
+  
+      init();
+  
+      deleteFiles(files);
+  
+      files.forEach(file => {
+        fs.copyFile(
+          path.join(filesPath, file),
+          path.join(copyFilesPath, file),
+          isErrorMessage
+        );
+      })
+  
+      console.log('\nMagic!\n');
+  
+    }
+  );
+
+}
+
+function init() {
+
+  fs.mkdir(
+    copyFilesPath,
+    {recursive: true},
+    isErrorMessage
+  );
+
+}
+
+function deleteFiles(files) {
+
+  fs.readdir(
+    copyFilesPath,
+    (error, copyFiles) => {
+
+      isErrorMessage(error);
+
+      let filesArray = copyFiles;
+
+      files.forEach(file => {
+        filesArray = filesArray.filter(item => item !== file);
+      });
+
+      filesArray.forEach(file => {
+        fs.unlink(path.join(copyFilesPath, file), isErrorMessage)
+      });
+
+    }
+  );
+
+}
+
+function isErrorMessage(error) {
   if (error) return console.error(error.message);
-
-  files.forEach(file => {
-    fs.copyFile(
-      path.join(filesPath, file),
-      path.join(copyFilesPath, file),
-      error => {
-        if (error) return console.error(error.message);
-      }
-    )
-  })
-
-})
+}
