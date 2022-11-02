dab776921f0ac60f5527153df1109d37081952e5
unknown <sunsn4ke@gmail.com>
2022-11-02 12:22:07 +0200

    feat: implement creating folder and copying files to it

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const path = require('path');
+const fs = require('fs');
 
+const filesPath = path.join(__dirname, 'files');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, error => {
+  if (error) return console.error(error.message);
+});
+
+const copyFilesPath = path.join(__dirname, 'files-copy');
+
+fs.readdir(filesPath, (error, files) => {
+
+  if (error) return console.error(error.message);
+
+  files.forEach(file => {
+    fs.copyFile(
+      path.join(filesPath, file),
+      path.join(copyFilesPath, file),
+      error => {
+        if (error) return console.error(error.message);
+      }
+    )
+  })
+
+})
