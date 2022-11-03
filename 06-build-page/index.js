23ec8d5b727a5888f0f81c49a28a8fca6a6a857a
unknown <sunsn4ke@gmail.com>
2022-11-03 12:49:53 +0200

    feat: implement writeHTML and writeStyles funcs

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,3 +1,134 @@
 const path = require('path');
 const fs = require('fs');
 
+const projectDistPath = path.join(__dirname, 'project-dist');
+const templatePath = path.join(__dirname, 'template.html');
+const componentsPath = path.join(__dirname, 'components');
+const stylesFolder = path.join(__dirname, 'styles');
+
+init();
+writeHTML();
+writeStyles();
+
+function writeHTML() {
+
+  fs.readFile(
+    templatePath,
+    {encoding: 'utf8'},
+    (error, data) => {
+  
+      isErrorMessage(error);
+  
+      let template = data;
+  
+      const templateSelectors = data.match(/{{\w+}}/gi)
+        .map(str => str.replace(/[{}]/gi, ''));
+
+      templateSelectors.forEach((item, index, arr) => {
+
+        const input = fs.createReadStream(
+          path.join(componentsPath, `${item}.html`),
+          'utf-8'
+        )
+  
+        let componentData = '';
+
+        input.on('data', chunk => componentData += chunk);
+        input.on('error', error => console.error(error.message));
+        input.on('end', () => {
+
+          template = template.replace(`{{${item}}}`, componentData);
+
+          if (index === arr.length - 1) {
+  
+            fs.writeFile(
+              path.join(projectDistPath, 'index.html'),
+              template,
+              isErrorMessage
+            );
+
+          }
+
+          })
+      });
+      
+    }
+  );
+
+}
+
+function writeStyles() {
+
+  fs.readdir(
+    stylesFolder,
+    {withFileTypes: true},
+    (error, files) => {
+  
+      isErrorMessage(error);
+  
+      const filesArray = [];
+  
+      files.forEach(file => {
+  
+        if (file.isFile() && path.extname(path.join(stylesFolder, file.name)) === '.css') {
+  
+          filesArray.push(file.name);
+  
+        }
+  
+      });
+  
+      const dataArray = [];
+  
+      filesArray.forEach((file, index) => {
+  
+        const input = fs.createReadStream(
+          path.join(stylesFolder, file),
+          'utf-8'
+        );
+  
+        let data = '';
+  
+        input.on('data', chunk => data += chunk);
+        input.on('error', error => console.error(error.message));
+        input.on('end', () => {
+  
+          dataArray.push(data);
+  
+          if (filesArray.length - 1 === index) {
+  
+            const newData = dataArray.join('');
+            const bundlePath = path.join(projectDistPath, 'style.css');
+  
+            fs.writeFile(
+              bundlePath,
+              newData,
+              isErrorMessage
+            );
+  
+          }
+  
+        });
+  
+      });
+  
+    }
+  );
+
+}
+
+function init() {
+
+  fs.mkdir(
+    projectDistPath,
+    {recursive: true},
+    error => {
+      if (error) return console.error('Error:\n', error.message);
+    }
+  );
+
+}
+
+function isErrorMessage(error) {
+  if (error) return console.error(error.message);
+}
