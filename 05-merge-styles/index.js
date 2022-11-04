965e4af076dddb102a3b792909ecac90984e0161
Dmytro Markuntovych <sunsn4ke@gmail.com>
2022-11-04 12:41:53 +0200

    fix: add function call

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,3 +4,5 @@ const mergeStyles = require(path.join(__dirname, 'mergeStyles'));
 
 const bundleFolder = path.join(__dirname, 'project-dist');
 const stylesFolder = path.join(__dirname, 'styles');
+
+mergeStyles(stylesFolder, bundleFolder, 'bundle.css');
